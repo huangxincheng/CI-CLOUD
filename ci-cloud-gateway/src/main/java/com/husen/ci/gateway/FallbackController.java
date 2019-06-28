365f33d86dd76ee0b7cd71f6f7bdb9b3b9a318e1
huangxincheng <huangxincheng@xmiles.cn>
2019-06-28 15:56:31 +0800

    修改

--- ci-cloud-gateway/src/main/java/com/husen/ci/gateway/FallbackController.java
+++ ci-cloud-gateway/src/main/java/com/husen/ci/gateway/FallbackController.java
@@ -3,16 +3,10 @@ package com.husen.ci.gateway;
 import com.husen.ci.framework.api.GlobalApiCode;
 import com.husen.ci.framework.api.GlobalApiResponse;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.core.io.buffer.DataBuffer;
-import org.springframework.core.io.buffer.DataBufferUtils;
+import org.springframework.cloud.gateway.support.ServerWebExchangeUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ServerWebExchange;
-import reactor.core.publisher.Flux;
-
-import java.nio.CharBuffer;
-import java.nio.charset.StandardCharsets;
-import java.util.concurrent.atomic.AtomicReference;
 
 /***
  @Author:MrHuang
@@ -25,12 +19,11 @@ import java.util.concurrent.atomic.AtomicReference;
 @Slf4j
 public class FallbackController {
 
-    private static final String HYSTRIX_EXCEPTION_ATTRIBUTE = "org.springframework.cloud.gateway.support.ServerWebExchangeUtils.hystrixExecutionException";
 
     @RequestMapping
     public GlobalApiResponse fallback(ServerWebExchange exchange) {
         // HystrixGatewayFilterFactory 将异常信息存入exchange的attribute中
-        log.error("Global Resposne" ,(Throwable)exchange.getAttributes().get(HYSTRIX_EXCEPTION_ATTRIBUTE));
+        log.error("Global Resposne" ,(Throwable)exchange.getAttributes().get(ServerWebExchangeUtils.HYSTRIX_EXECUTION_EXCEPTION_ATTR));
         GlobalHandlerCommon.handlerPost(exchange);
         return GlobalApiResponse.toFail(GlobalApiCode.GATEWAY_FALLBACK_CODE, GlobalApiCode.GATEWAY_FALLBACK_CODE_MSG);
     }
