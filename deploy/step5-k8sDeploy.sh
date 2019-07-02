53eb7ff303159c9945cd96286015386a8a901fe0
huangxincheng <huangxincheng@xmiles.cn>
2019-07-02 11:47:18 +0800

    修改脚本

--- deploy/step5-k8sDeploy.sh
+++ deploy/step5-k8sDeploy.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 deployAppName=$1
 deployK8sVersion=$2
-old=`grep 'image' /root/k8s-$deployAppName-deployment.yaml`
+deoloyAppDir=$3
+
+old=`grep 'image' /root/$deoloyAppDir/$deployAppName/k8s/k8s-$deployAppName-deployment.yaml`
 echo $old
 new="        image: registry-vpc.cn-shenzhen.aliyuncs.com/huangxincheng/$deployAppName:$deployK8sVersion"
 echo $new
 # 替换文件里面镜像
-sed -i "s!$old!$new!" /root/k8s-$deployAppName-deployment.yaml
+sed -i "s!$old!$new!" /root/$deoloyAppDir/$deployAppName/k8s/k8s-$deployAppName-deployment.yaml
 
 # 拉取镜像
 docker pull registry-vpc.cn-shenzhen.aliyuncs.com/huangxincheng/$deployAppName:$deployK8sVersion
 docker images -q registry-vpc.cn-shenzhen.aliyuncs.com/huangxincheng/$deployAppName:$deployK8sVersion
 
 # k8s部署
-kubectl apply -f /root/k8s-$deployAppName-deployment.yaml
-kubectl apply -f /root/k8s-$deployAppName-deployment-hpa.yaml
+kubectl apply -f /root/$deoloyAppDir/$deployAppName/k8s
