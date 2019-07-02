b2b7990460ae43998878ef1b05463f4eb913bc90
huangxincheng <huangxincheng@xmiles.cn>
2019-07-02 18:39:35 +0800

    x1

--- deploy/step7-recycleImage.sh
+++ deploy/step7-recycleImage.sh
@@ -4,4 +4,4 @@ deployAppName=$1
 deployAppVersion=$2
 
 # 回收资源命令（这里只回收master的 其他节点可扩展回收）
-docker images|grep "$deployAppName"|grep -v "$deployAppVersion"|awk -F ' ' '{print $3}'|xargs docker rmi
+docker images|grep "$deployAppName"|grep -v "$deployAppVersion"|awk -F ' ' '{print $3}'|xargs docker rmi -f
