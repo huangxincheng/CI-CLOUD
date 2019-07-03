5fefd4be2e06a91bee0cb3508082587fa31397fe
huangxincheng <huangxincheng@xmiles.cn>
2019-07-03 21:22:09 +0800

    修改cleanDockerImage.sh

--- deploy/cleanDockerImage.sh
+++ deploy/cleanDockerImage.sh
@@ -3,4 +3,5 @@ deployAppName=$1
 deployAppVersion=$2
 
 # 回收资源命令（这里只回收master的 其他节点可扩展回收）
+docker rmi $(docker images | grep "none" | awk '{print $3}')
 docker images|grep "registry.cn-shenzhen.aliyuncs.com/huangxincheng/$deployAppName"|grep -v "$deployAppVersion"|awk -F ' ' '{print $3}'|xargs docker rmi
