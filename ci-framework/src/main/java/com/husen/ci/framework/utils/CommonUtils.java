73220cd879a4484d04d48076addf950ee517304f
huangxincheng <huangxincheng@xmiles.cn>
2019-07-08 10:44:19 +0800

    移动类

---
+++ ci-framework/src/main/java/com/husen/ci/framework/utils/CommonUtils.java
@@ -0,0 +1,14 @@
+package com.husen.ci.framework.utils;
+
+/***
+ @Author:MrHuang
+ @Date: 2019/7/8 10:38
+ @DESC: TODO
+ @VERSION: 1.0
+ ***/
+public class CommonUtils {
+
+    public static boolean success(Object obj) {
+        return obj != null;
+    }
+}
