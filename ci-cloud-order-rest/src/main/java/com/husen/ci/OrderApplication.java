4b7d54fd2db5c127b1baf051ecd6ea0452c01627
huangxincheng <huangxincheng@xmiles.cn>
2019-07-08 18:57:03 +0800

    测试aop

--- ci-cloud-order-rest/src/main/java/com/husen/ci/OrderApplication.java
+++ ci-cloud-order-rest/src/main/java/com/husen/ci/OrderApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 /***
  @Author:MrHuang
@@ -16,6 +17,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableDiscoveryClient
 @EnableFeignClients
 @EnableCircuitBreaker
+@EnableAspectJAutoProxy(proxyTargetClass = true, exposeProxy = true)
 public class OrderApplication {
 
     public static void main(String[] args) {
