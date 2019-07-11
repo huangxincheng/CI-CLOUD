a327121a42493f7319aca0abcda3ec3492a330c5
huangxincheng <huangxincheng@xmiles.cn>
2019-07-11 15:47:28 +0800

    删除AuthGatewayFilterFactory

--- ci-cloud-gateway/src/main/java/com/husen/ci/gateway/GatewayHandlerCommon.java
+++ ci-cloud-gateway/src/main/java/com/husen/ci/gateway/GatewayHandlerCommon.java
@@ -1,28 +1,19 @@
 package com.husen.ci.gateway;
 
-import com.husen.ci.framework.api.GlobalApiCode;
 import com.husen.ci.framework.api.GlobalApiResponse;
-import com.husen.ci.framework.auth.JwtUtils;
 import com.husen.ci.framework.json.JSONUtils;
 import io.netty.buffer.ByteBufAllocator;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.cloud.gateway.route.Route;
 import org.springframework.cloud.gateway.support.ServerWebExchangeUtils;
 import org.springframework.core.io.buffer.DataBuffer;
 import org.springframework.core.io.buffer.NettyDataBufferFactory;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.web.server.ServerWebExchange;
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 
 import java.net.URI;
 import java.nio.charset.StandardCharsets;
 import java.util.LinkedHashSet;
-import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 
@@ -57,92 +48,6 @@ class GatewayHandlerCommon {
     }
 
     /**
-     * 判断是否Pass认证
-     * @param exchange
-     * @return
-     */
-    static boolean isPassAuth(ServerWebExchange exchange, AuthGatewayFilterFactory.Config config) {
-        List<String> authList = exchange.getRequest().getHeaders().get(PASS_AUTH);
-        if (authList != null && authList.size() > 0) {
-            return true;
-        }
-        String authParam = exchange.getRequest().getQueryParams().getFirst(PASS_AUTH);
-        if (authParam != null) {
-            return true;
-        }
-        // 获取当前调整的route
-        return isIgnoreAuth(exchange, config);
-    }
-
-    /**
-     * 是否是忽略认证
-     * @param exchange
-     * @param config
-     * @return
-     */
-    private static boolean isIgnoreAuth(ServerWebExchange exchange, AuthGatewayFilterFactory.Config config) {
-        Route route = exchange.getAttribute(ServerWebExchangeUtils.GATEWAY_ROUTE_ATTR);
-        if (route == null) {
-            return true;
-        }
-        // 判断是否在不需要认证的服务列表中
-        List<String> notAuthService = config.getNotAuthService();
-        if (notAuthService != null && notAuthService.contains(route.getId())) {
-            return true;
-        }
-        // 判断是否在不需要认证的请求uri中
-        String rawPath = exchange.getRequest().getURI().getRawPath();
-        List<AuthGatewayFilterFactory.ServiceRoute> ignore = config.getIgnore();
-        if (ignore != null && ignore.size() > 0) {
-            for (AuthGatewayFilterFactory.ServiceRoute serviceRoute : ignore) {
-                if (StringUtils.equals(serviceRoute.getService(), route.getId())) {
-                    for (String uri : serviceRoute.getUri()) {
-                        if (rawPath.startsWith(uri)) {
-                            return true;
-                        }
-                    }
-                }
-            }
-        }
-        return false;
-    }
-
-    /**
-     * 判断是否正常成功
-     * @param exchange
-     * @return
-     */
-    static boolean isAuth(ServerWebExchange exchange) {
-        List<String> authTokenList = exchange.getRequest().getHeaders().get(AUTH_TOKEN);
-        String authToken;
-        if (authTokenList != null && authTokenList.size() > 0) {
-             authToken = authTokenList.get(0);
-        } else {
-            authToken = exchange.getRequest().getQueryParams().getFirst(AUTH_TOKEN);
-        }
-        //TODO 这里只简单判断校验就行, 后面可以增加判断Redis中是否存在
-        return JwtUtils.checkToken(authToken);
-    }
-
-    /**
-     * 返回认证失败
-     * @param exchange
-     * @return
-     */
-    static Mono<Void> returnAuthFail(ServerWebExchange exchange) {
-        // 不能pass的直接失败
-        GlobalApiResponse rsp = GlobalApiResponse.toFail(GlobalApiCode.UNAUTH_CODE, GlobalApiCode.UNAUTH_CODE_MSG);
-        exchange.getResponse().setStatusCode(HttpStatus.OK);
-        exchange.getResponse().getHeaders().set(HttpHeaders.CONTENT_LENGTH, JSONUtils.object2Bytes(rsp).length + "");
-        exchange.getResponse().getHeaders().set(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_UTF8_VALUE);
-        return exchange.getResponse()
-                .writeWith(
-                        Flux.just(GatewayHandlerCommon.getDataBuffer(exchange.getResponse(), rsp))
-                );
-    }
-
-
-    /**
      * 将result转换成DataBuffer
      * @param response
      * @param result
@@ -151,8 +56,6 @@ class GatewayHandlerCommon {
     private static DataBuffer getDataBuffer(ServerHttpResponse response, GlobalApiResponse result) {
         return response.bufferFactory().wrap(JSONUtils.object2Bytes(result));
     }
-
-
     /**
      * 将result转换成DataBuffer
      * @param result
