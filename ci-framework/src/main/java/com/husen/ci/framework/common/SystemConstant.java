03aa16fdced45345e6320a8994460dcc43669596
huangxincheng <huangxincheng@xmiles.cn>
2019-07-12 15:29:29 +0800

    修改

--- ci-framework/src/main/java/com/husen/ci/framework/common/SystemConstant.java
+++ ci-framework/src/main/java/com/husen/ci/framework/common/SystemConstant.java
@@ -8,13 +8,28 @@ package com.husen.ci.framework.common;
  ***/
 public class SystemConstant {
 
+    /**
+     * HTTP ContentType 中 JSON
+     */
     public static final String APPLICATION_JSON_UTF8_VALUE = "application/json;charset=UTF-8";
 
+    /**
+     * HTTP ContentType 中 URLENCODED
+     */
     public static final String APPLICATION_FORM_URLENCODED_VALUE = "application/x-www-form-urlencoded";
 
+    /**
+     * HTTP STATUS OK
+     */
     public static final int HTTP_CODE_OK = 200;
 
+    /**
+     * TIME FORMAT
+     */
     public static final String FORMAT_TIME_PATTERN = "yyyy-MM-dd HH:mm:ss";
 
+    /**
+     * DATE FORMAT
+     */
     public static final String FORMAT_DATE_PATTERN = "yyyy-MM-dd";
 }
