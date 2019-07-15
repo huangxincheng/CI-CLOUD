08f68218a98b84868fd2355a36ef934e43f1aa77
huangxincheng <huangxincheng@xmiles.cn>
2019-07-15 14:19:48 +0800

    sso server增加cors 跨域请求

--- ci-cloud-sso-server/src/main/java/com/husen/ci/sso/WebMvcConfig.java
+++ ci-cloud-sso-server/src/main/java/com/husen/ci/sso/WebMvcConfig.java
@@ -1,14 +1,15 @@
 package com.husen.ci.sso;
 
-import com.husen.ci.framework.api.WebMvcConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * @Author husen
+ * addCorsMappings 配置CORS跨域问题1
  */
 @Configuration
-public class WebMvcConfig extends WebMvcConfiguration {
+public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
