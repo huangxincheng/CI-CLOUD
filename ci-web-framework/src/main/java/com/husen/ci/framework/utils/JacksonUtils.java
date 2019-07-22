3b59da36c39fe754e51e939ed5755b4972d3d094
huangxincheng <huangxincheng@xmiles.cn>
2019-07-22 14:44:25 +0800

    jacksonUtils

---
+++ ci-web-framework/src/main/java/com/husen/ci/framework/utils/JacksonUtils.java
@@ -0,0 +1,86 @@
+package com.husen.ci.framework.utils;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/***
+ @Author:MrHuang
+ @Date: 2019/7/22 14:41
+ @DESC: TODO
+ @VERSION: 1.0
+ ***/
+public class JacksonUtils {
+
+    private final static ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+
+    private JacksonUtils() {}
+
+    public static ObjectMapper getInstance() {
+        return OBJECT_MAPPER;
+    }
+
+    /**
+     * javaBean,list,array convert to json string
+     */
+    public static String obj2json(Object obj) throws Exception {
+        return OBJECT_MAPPER.writeValueAsString(obj);
+    }
+
+    /**
+     * json string convert to javaBean
+     */
+    public static <T> T json2pojo(String jsonStr, Class<T> clazz)
+            throws Exception {
+        return OBJECT_MAPPER.readValue(jsonStr, clazz);
+    }
+
+    /**
+     * json string convert to map
+     */
+    public static <T> Map<String, Object> json2map(String jsonStr)
+            throws Exception {
+        return OBJECT_MAPPER.readValue(jsonStr, Map.class);
+    }
+
+    /**
+     * json string convert to map with javaBean
+     */
+    public static <T> Map<String, T> json2map(String jsonStr, Class<T> clazz)
+            throws Exception {
+        Map<String, Map<String, Object>> map = OBJECT_MAPPER.readValue(jsonStr,
+                new TypeReference<Map<String, T>>() {
+                });
+        Map<String, T> result = new HashMap<String, T>();
+        for (Map.Entry<String, Map<String, Object>> entry : map.entrySet()) {
+            result.put(entry.getKey(), map2pojo(entry.getValue(), clazz));
+        }
+        return result;
+    }
+
+    /**
+     * json array string convert to list with javaBean
+     */
+    public static <T> List<T> json2list(String jsonArrayStr, Class<T> clazz)
+            throws Exception {
+        List<Map<String, Object>> list = OBJECT_MAPPER.readValue(jsonArrayStr,
+                new TypeReference<List<T>>() {
+                });
+        List<T> result = new ArrayList<T>();
+        for (Map<String, Object> map : list) {
+            result.add(map2pojo(map, clazz));
+        }
+        return result;
+    }
+
+    /**
+     * map convert to javaBean
+     */
+    public static <T> T map2pojo(Map map, Class<T> clazz) {
+        return OBJECT_MAPPER.convertValue(map, clazz);
+    }
+}
